c3447b134f7b4ec464f05bdd0db20ed52a0bcc1a
Brian Makhembu <31996990+makhembu@users.noreply.github.com>
2021-09-17 02:45:55 +0300

    Update xyzlimited.py

--- xyzlimited.py
+++ xyzlimited.py
@@ -1,52 +1,91 @@
-# works with both python 2 and 3
-from __future__ import print_function
-import africastalking
-from pandas import *
+import africastalking #send airtime API
+import csv  #python inbuild csv module
+import re # regex library
 
-# reading CSV file
-data = read_csv("xyz_employees.csv")
-#adding names to list
-name = data['employee_name'].tolist()
-#adding phone numbers to list
-phone = data['employee_phone_number'].tolist()
-#adding airtime to list
-airtime = data['airtime_amount'].tolist()
-formated_number = []
+username = "sandbox" #AfricasTalking username
+api_key = "36061b0a6786f5561d5a01c9fe5ecbea09ab096597ba49fff2989c873ff6f4d1" #Africastalking password
 
-class AIRTIME:
-    def __init__(self):
-        # Set your AfricasTalking credentials
-        self.username = "usenname"
-        self.api_key = "password"
 
-        # Initialize the SDK
-        africastalking.initialize(self.username, self.api_key)
+phone = [] #list to collect phone numbers from csv
+airtime_amount = [] #list to collect airtime amount from csv
+formatted_number = [] #list to store formatted format numbers
+formatted_airtime_amount = [] #list to store cleaned up amount 
 
-        # Get the airtime service
-        self.airtime = africastalking.Airtime
-    
-    
+# Reading csv data into the lists above
+with open('xyz_employees.csv', 'r') as csv_file:
+    reader = csv.DictReader(csv_file, delimiter=',') # using dict reader because csv has columns
+    for lines in reader:
+        phone.append(lines['employee_phone_number']) #appending phone numbers to list
+        airtime_amount.append(lines['airtime_amount']) #appending airtime amount to list
 
-    def send(self):
-        #loop through phone numbers
-        for i in range(0,len(phone)):
-            # adding a try/catch to break the loop if an error is found
-            try:
-                # Set phone_number in international format
-                phone_number = phone_number[i]
+#function to convert phone number to international format which is needed by AfricasTalking
+def format_international():
+    for num in phone: #iterate list
+        num = re.sub("[^0-9]", "", num) #regex to extract digits in the phone number string 
+        x=num.startswith('254') #checks number prefix
+        y =num.startswith('0')  #checks number prefix
+        z = num.startswith('7')  #checks number prefix
 
-                # Set The 3-Letter ISO currency code and the amount
-                amount = airtime[i]
-                currency_code = "KES"
+        if x is True:   #validates  xprefix condition
+            form = '+' + num    #adds a + prefix to the number
+            if (len(form) == 13):   #checks if number is valid
+                formatted_number.append(form)
+                print('formatted: ' + str(form))
+            else:
+                print(form + ' is an invalid number please check again!')
 
-                try:
-                    # That's it hit send and we'll take care of the rest
-                    responses = self.airtime.send(phone_number=phone_number, amount=amount, currency_code=currency_code)
-                    print (responses)
-                except Exception as e:
-                    print ("Encountered an error while sending airtime:%s" %str(e))
-            except:
-                break 
+        elif y is True:     #validates y prefix condition
+            num = num[1:]   #removes first digit of number
+            form = '+254' + num #adds a +254 prefix
+            if (len(form) == 13): #checks if number is valid
+                formatted_number.append(form)
+                print('formatted: ' + str(form))
+            else:
+                print(form + ' is an invalid number please check again!')
 
-if __name__ == '__main__':
-    AIRTIME().send()
+        elif z is True: #validates z prefix condition
+            form = '+254' + num #adds a +254 prefix
+            if (len(form) == 13):   #checks if number is valid
+                formatted_number.append(form)
+                print('formatted: ' + str(form))
+            else:
+                print(form + ' is an invalid number please check again!')
+
+format_international()
+
+#function to cleanup the amount data. extract numbers
+def clean_amount(): 
+    for i in airtime_amount:
+        regex = '[+-]?[0-9]+\.[0-9]+' # regex to check for floats
+        if (re.search(regex, i)):   #check if float condition is True
+            formatted_airtime_amount.append(i)  #appends amount to list
+        else:
+            i =  re.sub("[^0-9]", "", i)    #if number is not a float, extract only digits
+            formatted_airtime_amount.append(i)
+
+clean_amount()  #call function
+
+# Initialize the SDK
+africastalking.initialize(username, api_key)
+
+
+# Get the airtime service
+airtime = africastalking.Airtime
+
+i=0
+for i in range(0,len(formatted_number)):
+    # phone_number in international format
+    phone_number = formatted_number[i]
+
+    # Set The 3-Letter ISO currency code and the cleaned amount
+    currency_code = "KES"
+    amount = formatted_airtime_amount[i]
+
+    try:
+        # Send data to API
+        response = airtime.send(phone_number=phone_number, amount=amount, currency_code=currency_code)
+        print(response)
+    except Exception as e:
+        print(f"Encountered an error while sending airtime. More error details below\n {e}")
+
+
